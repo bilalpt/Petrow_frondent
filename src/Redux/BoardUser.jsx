d7cc3a5f5470f03ebd52cafe2312dbcf500209ca
bilalpt <bilalpt572@gmail.com>
2023-10-10 19:17:35 +0530

    redux just lurn and redux board user created

---
+++ src/Redux/BoardUser.jsx
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState={
+    id:"",
+    username:"",
+    email:"",
+    phone:"",
+
+};
+
+const Boardslice=createSlice({
+
+    name:"boarduser",
+    initialState,
+    reducers:{
+        
+
+    }
+
+
+
+})
+
+
+
+
